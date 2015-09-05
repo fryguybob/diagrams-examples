e304a0569c832395d96bd9ab106ff19e9b156508
Ryan Yates <ryates@cs.rochester.edu>
2015-09-04 20:34:54 -0400

    Factored out sorting and coloring.

--- tree/Tree.hs
+++ tree/Tree.hs
@@ -10,6 +10,9 @@ import Control.Monad
 import Control.Monad.Random
 
 import Data.Colour.Palette.BrewerSet
+import qualified Data.Foldable as F
+import Data.List
+import Data.Ord
 
 import Diagrams.Prelude hiding (size)
 import Diagrams.Backend.Rasterific.CmdLine
@@ -34,6 +37,9 @@ levels t = takeWhile (/= 0) $ go t
     go (Leaf _) = 1 : repeat 0
     go (Branch _ l r) = 1 : zipWith (+) (go l) (go r)
 
+depth (Leaf _) = 0
+depth (Branch _ l r) = 1 + max (depth l) (depth r)
+
 depthByFactor t p = go ls (floor (fromIntegral s * p)) 0
   where
     ls = levels t
@@ -43,12 +49,6 @@ depthByFactor t p = go ls (floor (fromIntegral s * p)) 0
     go (a:as) t n 
         | a < t     = go as (t-a) (n+1)
         | otherwise = n
-
-
-labelDepth t = go t 0
-  where
-    go (Leaf _)       n = Leaf n
-    go (Branch _ l r) n = Branch n (go l (n+1)) (go r (n+1))
 
 stickZ p = origin ~~ (0 ^& 0 ^& 1)
         <> p # translate unitZ
@@ -82,20 +82,49 @@ tree3DWibble (Branch _ l r) = do
     vr <- scale 0.9 . rotX ((-tilt) @@ deg) . rotZ (90 @@ deg) <$> tree3DWibble r
 
     veeWibble vl vr
-
+{-
 veeWibble' f = do
      lf <- (+0.8) . (*0.4) <$> getRandom
      rf <- (+0.8) . (*0.4) <$> getRandom
-     return $ stickZ mempty # scale lf # rotX (  branch  @@ deg) # f
-           <> stickZ mempty # scale rf # rotX ((-branch) @@ deg) # f
+     let u = unitZ # scale lf # rotX (  branch  @@ deg)
+         v = unitZ # scale rf # rotX ((-branch) @@ deg)
+     return ( f (origin ~~ u) <> f (origin ~~ v)
+            , undefined -- f (origin .+^ u)
+            , undefined -- f (origin .+^ v)
+            )
 
 tree3DWibble' f (Leaf _)       = return $ Leaf (f mempty)
-tree3DWibble' f (Branch _ l r) =
-    Branch <$> veeWibble' f
-           <*> tree3DWibble' (f . scale 0.9 . rotX (  tilt  @@ deg) . rotZ (90 @@ deg)) l
-           <*> tree3DWibble' (f . scale 0.9 . rotX ((-tilt) @@ deg) . rotZ (90 @@ deg)) r
+tree3DWibble' f (Branch _ l r) = do
+    (w,ol,or) <- veeWibble' f
+    Branch w
+           <$> tree3DWibble' (f . translate ol . scale 0.9 . rotX (  tilt  @@ deg) . rotZ (90 @@ deg)) l
+           <*> tree3DWibble' (f . translate or . scale 0.9 . rotX ((-tilt) @@ deg) . rotZ (90 @@ deg)) r
+-}
 
-m  = lookAt (V3 8.4 6 3.2) zero unitZ
+veeWibble' o f = do
+    lf <- (+0.8) . (*0.4) <$> getRandom
+    rf <- (+0.8) . (*0.4) <$> getRandom
+    let vl = (origin ~~ (origin .+^ unitZ)) # scale lf # rotX (  branch  @@ deg) # f
+        vr = (origin ~~ (origin .+^ unitZ)) # scale rf # rotX ((-branch) @@ deg) # f 
+    return ( trailLike (vl `at` o)
+          <> trailLike (vr `at` o)
+           , o .+^ trailOffset vl
+           , o .+^ trailOffset vr
+           , f . rotX (  branch  @@ deg) . scale lf
+           , f . rotX ((-branch) @@ deg) . scale rf
+           )
+
+tree3DWibble' :: Tree a -> IO (Tree (Path V3 Double))
+tree3DWibble' t = go origin id t
+  where
+    go _ _ (Leaf _)       = return $ Leaf mempty
+    go o f (Branch _ l r) = do
+      (w,lo,ro,fl,fr) <- veeWibble' o f
+      Branch w <$> go lo (fl . scale 0.9 . rotX (  tilt  @@ deg) . rotZ (90 @@ deg)) l
+               <*> go ro (fr . scale 0.9 . rotX ((-tilt) @@ deg) . rotZ (90 @@ deg)) r
+
+viewVector = -(V3 8.4 6 3.2)
+m  = lookAt (-viewVector) zero unitZ
 pm = perspective (pi/3) 0.8 (-10) 10 !*! m
 
 pd = m44Deformation pm
@@ -109,9 +138,6 @@ traceShowId a = T.trace (show a) a
 
 treeColors t = go t 0
   where
-    depth (Leaf _) = 0
-    depth (Branch _ l r) = 1 + max (depth l) (depth r)
-
     m = depth t
     m' = depthByFactor t 0.3
     cs = brewerSet RdYlGn 9
@@ -125,22 +151,48 @@ withPerspective :: [Kolor] -> Path V3 Double -> Diagram B
 withPerspective cs d = mconcat . zipWith (\c -> lc c . strokeLocT) cs . concat
                      . explodePath $ deform pd (translateZ (-1) d)
 
+getCenter = boxCenter . boundingBox
+
 tree2D t r = tree3D t # rotZ r # withPerspective (treeColors t)
 
 tree2D' t r = tree3D t # fmap (rotZ r) # fmap withPerspective'
+
+colorByDepth t = go t 0
+  where
+    m = depth t
+    m' = depthByFactor t 0.3
+    cs = brewerSet RdYlGn 9
+
+    color n = cs !! (floor (fromIntegral (n * 8) / fromIntegral m') `min` 8)
+
+    go (Leaf x)        d = Leaf (color d, x)
+    go (Branch x l r ) d = Branch (color d, x) (go l (d+1)) (go r (d+1))
+
+
+spinAndProject t = \r -> fmap ((\x -> (getCenter x, withPerspective' x)) . rotZ r) t
+
+to2D = fmap (mconcat . sortZ . F.toList . colorByDepth) . spinAndProject
+    where
+        sortZ = map applyColor . sortBy s
+
+        s = comparing (fmap (dot v . (.-. p)) . fst . snd)
+        p = origin .+^ (-viewVector)
+        v = signorm viewVector
+
+        applyColor (k,(_,d)) = d # lc k
 
 tree2DWibble t = do
     w <- tree3DWibble t 
     return $ \r -> w # rotZ r # withPerspective (treeColors t)
 
-tree2DWibble' t = do
-    w <- tree3DWibble' id t
-    return $ \r -> w # fmap (rotZ r) # fmap withPerspective'
+frameCount = 100
+delay = 6
 
-spin f = map (,6) . allRotations $ frame
+spin f = map (,delay) . allRotations $ frame
   where
     !bb = boundingBox . mconcat . allRotations $ f
-    allRotations f = [f (n @@ turn) | i <- [0..100], let n = fromIntegral i / 100]
+    allRotations f = [f (n @@ turn) | i <- [0..frameCount]
+                                    , let n = fromIntegral i / fromIntegral frameCount]
     frame r = f r # withEnvelope bb # bgFrame 0.05 skyblue
 
 sky bb = mkLinearGradient (mkStops [(darkgreen,0,1), (white,0.1,1), (skyblue,1,1)])
@@ -173,9 +225,12 @@ mainRandWibble = do
     putStrLn "Tree built"
 
     putStrLn $ "Size: " ++ show (size t)
-    w <- tree2DWibble t
+--     w <- tree2DWibble t
+--    w <- to2D <$> tree3DWibble' (full 8)
+    w <- to2D <$> tree3DWibble' t
     gifMain (spin w)
-    -- mainWith (w (0 @@ turn))
+
+--    mainWith (w (0 @@ turn))
 
 mainFrames = mainWith (map fst $ spin (tree2D t))
   where
